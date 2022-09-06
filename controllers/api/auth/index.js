56201c28e49980326349fa6e8526299fbe7ceeb7
Brad Teague <b_rad88@live.com>
2022-09-06 21:53:01 +0930

    Create home and auth route skeletons

---
+++ controllers/api/auth/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.post("/login", (req, res) => {
+  // TODO
+});
+
+router.post("/logout", (req, res) => {
+  // TODO
+});
+
+module.exports = router;
