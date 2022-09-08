12f653e3803aaccecc3579dfce51d5ed737a028f
Brad Teague <b_rad88@live.com>
2022-09-08 19:23:25 +0930

    Create new user

--- controllers/api/auth/index.js
+++ controllers/api/auth/index.js
@@ -1,4 +1,22 @@
 const router = require("express").Router();
+const { User } = require("../../../model");
+
+router.post("/", async (req, res) => {
+  // Extract details from req.body for validation
+  const { first_name, last_name, email, password } = req.body;
+  if (!first_name || !last_name || !email || !password) {
+    return res.sendStatus(400);
+  }
+  // Check if email already exists in database, if it does return 'bad request'
+  const user = await User.findOne({ where: { email } });
+  if (user) {
+    return res.sendStatus(400);
+  }
+
+  // Create the user
+  const newUser = await User.create(req.body);
+  res.json({ newUser });
+});
 
 router.post("/login", (req, res) => {
   // TODO
