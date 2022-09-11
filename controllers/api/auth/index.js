6a85fc12de677ff1ddea58f06a33466e453f2e11
Brad Teague <b_rad88@live.com>
2022-09-12 00:09:26 +0930

    Signup new user with auth routing

--- controllers/api/auth/index.js
+++ controllers/api/auth/index.js
@@ -3,42 +3,60 @@ const { User } = require("../../../model");
 
 router.post("/", async (req, res) => {
   // Extract details from req.body for validation
-  const { first_name, last_name, email, password } = req.body;
-  if (!first_name || !last_name || !email || !password) {
-    return res.sendStatus(400);
-  }
-  // Check if email already exists in database, if it does return 'bad request'
-  const user = await User.findOne({ where: { email } });
-  if (user) {
-    return res.sendStatus(400);
-  }
+  try {
+    const { first_name, last_name, email, password } = req.body;
+    if (!first_name || !last_name || !email || !password) {
+      return res.sendStatus(400);
+    }
+    // Check if email already exists in database, if it does return 'bad request'
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+      return res.sendStatus(400);
+    }
 
-  // Create the user
-  const newUser = await User.create(req.body);
-  res.json({ newUser });
+    // Create the user
+    const newUser = await User.create(req.body);
+    // Return new user data
+    if (newUser) {
+      req.session.save(err => {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        req.session.user_id = newUser.id;
+        req.session.logged_in = true;
+        res.sendStatus(201);
+      });
+    }
+  } catch (error) {
+    res.sendStatus(400);
+  }
 });
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.sendStatus(400);
-  }
-  const userData = await User.findOne({ where: { email } });
-  if (!userData) {
-    return res.sendStatus(400);
-  }
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+    const userData = await User.findOne({ where: { email } });
+    if (!userData) {
+      return res.sendStatus(400);
+    }
 
-  const validPassword = await userData.checkPassword(password);
+    const validPassword = await userData.checkPassword(password);
 
-  if (validPassword) {
-    req.session.save(err => {
-      if (err) {
-        return res.sendStatus(500);
-      }
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      return res.status(200).json({ user: userData });
-    });
+    if (validPassword) {
+      req.session.save(err => {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        return res.sendStatus(200);
+      });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
