e69bfa2a60c74d0eb73db9ceb67b19dbf8a4f9bc
Brad Teague <b_rad88@live.com>
2022-09-13 12:00:00 +0930

    Create api route for creating/updating character avatar

--- controllers/api/user/characters.js
+++ controllers/api/user/characters.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const multer = require("multer")().single("image");
+const resize = require("../../../utils/imageResize");
+const upload = require("../../../utils/s3");
 const { Character } = require("../../../model");
+const HandledError = require("../../../error/Error");
 
 router.post("/", async (req, res, next) => {
   try {
@@ -8,6 +12,27 @@ router.post("/", async (req, res, next) => {
     res.json(character);
   } catch (error) {
     next(error);
+  }
+});
+
+router.post("/image/:char_id", multer, async (req, res, next) => {
+  try {
+    const { user_id } = req.session;
+    const { char_id } = req.params;
+    const key = `${user_id}.${char_id}.png`;
+    const image = req.file;
+    if (!image) {
+      return next(HandledError.badRequest());
+    }
+    const character = await Character.findByPk(char_id);
+    if (!character) return next(HandledError.badRequest());
+    const resizedImage = await resize(image.buffer);
+    const location = await upload(resizedImage, key);
+    await character.update({ avatar: location });
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    next(HandledError.unknownError());
   }
 });
 
