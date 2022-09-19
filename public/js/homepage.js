67fd58f96df64e4217836caa7316299ad4791818
Brad Teague <b_rad88@live.com>
2022-09-19 16:12:04 +0930

    Error handling on attribute fetch request

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -8,22 +8,26 @@ const editPreview = document.getElementById("selected-character-edit");
 const previewDiv = document.getElementById("preview");
 
 characterList.addEventListener("click", async e => {
-  const data = e.target.dataset;
-  if (!data.id) return;
+  try {
+    const data = e.target.dataset;
+    if (!data.id) return;
 
-  const res = await fetch(`/api/user/character/attributes/${data.id}`);
+    const res = await fetch(`/api/user/character/attributes/${data.id}`);
 
-  const attributes = await res.json();
-  previewDiv.removeAttribute("hidden");
-  namePreview.innerText = data.name;
-  racePreview.innerText = data.race;
-  classPreview.innerText = data.class;
-  avatarPreview.src = data.avatar;
-  bioPreview.innerText = data.bio;
-  editPreview.setAttribute("href", `/update/${data.id}`);
-  attributes.forEach(attribute => {
-    const id = `selected-character-attribute-${attribute.attribute_id}`;
-    const element = document.getElementById(id);
-    element.innerText = attribute.value;
-  });
+    const attributes = await res.json();
+    previewDiv.removeAttribute("hidden");
+    namePreview.innerText = data.name;
+    racePreview.innerText = data.race;
+    classPreview.innerText = data.class;
+    avatarPreview.src = data.avatar;
+    bioPreview.innerText = data.bio;
+    editPreview.setAttribute("href", `/update/${data.id}`);
+    attributes.forEach(attribute => {
+      const id = `selected-character-attribute-${attribute.attribute_id}`;
+      const element = document.getElementById(id);
+      element.innerText = attribute.value;
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
