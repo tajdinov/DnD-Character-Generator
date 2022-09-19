d3c83f606da49783b35f3f1b8f3e4b0e46f7ca8a
Brad Teague <b_rad88@live.com>
2022-09-19 11:27:03 +0930

    Allow connection url to initialise sequelize

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,18 @@ require("dotenv").config();
 
 let sequelize;
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, CLEARDB_DATABASE_URL } =
+  process.env;
 
-sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "mysql",
-  port: 3306,
-});
+if (CLEARDB_DATABASE_URL) {
+  console.log("Attempting to connect with connection url");
+  sequelize = new Sequelize(CLEARDB_DATABASE_URL);
+} else {
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "mysql",
+    port: 3306,
+  });
+}
 
 module.exports = sequelize;
