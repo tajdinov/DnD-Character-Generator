bb3cf8ac0b508ec7f190b19c0cbb33ec6677f021
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2023-02-13 14:33:12 +1030

    commit

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (CLEARDB_DATABASE_URL) {
   sequelize = new Sequelize(CLEARDB_DATABASE_URL);
 } else {
   sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
+    host: "localhost",
     dialect: "mysql",
     port: 3306,
   });
